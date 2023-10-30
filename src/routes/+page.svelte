2ca6f8d790afb18756e18bcdbdf1be0e7d26709a
Leangeful <cuttheleash1@gmail.com>
2023-10-30 19:36:54 +0100

    fonts added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1 mt-5">RealmMart</h1>
 		<p>Start by exploring:</p>
+
+		<div class="h3">Stores</div>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
 			<li><code class="code">/src/app.postcss</code> - app wide css</li>
