7ab64ba0f5720f30e082ca463601ac4caab4809c
Leangeful <cuttheleash1@gmail.com>
2023-11-02 14:01:32 +0100

    seperate translations

--- src/lib/i18n/translations.ts
+++ src/lib/i18n/translations.ts
@@ -1,44 +1,7 @@
-import lang from './lang.json';
+import { de } from './translations/de';
+import { en } from './translations/en';
 
 export default {
-	en: {
-		lang,
-		nav: {
-			start: 'Main',
-			settings: 'Settings'
-		},
-		content: {
-			title: 'Welcome to SvelteKit',
-			info: 'You can now access all your language mutations using <code>{$l(LOCALE, TRANSLATION_KEY)}</code>:',
-			text: 'Visit <a href="{{link}}">kit.svelte.dev</a> to read the documentation'
-		}
-	},
-	de: {
-		lang,
-		nav: {
-			start: 'Start',
-			settings: 'Einstellungen'
-		},
-		content: {
-			title: 'Willkommen SvelteKit',
-			info: 'You can now access all your language mutations using <code>{$l(LOCALE, TRANSLATION_KEY)}</code>:',
-			text: 'Visit <a href="{{link}}">kit.svelte.dev</a> to read the documentation'
-		}
-	}
+	en,
+	de
 };
-
-/* export default {
-	en: {
-		common: {
-			Key: 'Blah',
-			Hello: 'Hello',
-			Start: 'Start by exploring'
-		}
-	},
-	de: {
-		common: {
-			Hello: 'Hallo',
-			Start: 'Fange an zu erkunden'
-		}
-	}
-}; */
---
+++ src/lib/i18n/translations/en.ts
@@ -0,0 +1,13 @@
+import lang from '../lang.json';
+export const en = {
+	lang,
+	nav: {
+		start: 'Main',
+		settings: 'Settings'
+	},
+	content: {
+		title: 'Welcome to SvelteKit',
+		info: 'You can now access all your language mutations using <code>{$l(LOCALE, TRANSLATION_KEY)}</code>:',
+		text: 'Visit <a class="text-secondary-700 hover:text-secondary-300" href="{{link}}">kit.svelte.dev</a> to read the documentation'
+	}
+};
