7f062cdb0cc9822fd32896eb1d3fdd97a413d872
Leangeful <cuttheleash1@gmail.com>
2023-11-02 16:24:09 +0100

    prevent multiple workers

--- src/lib/game/GameService.svelte
+++ src/lib/game/GameService.svelte
@@ -2,6 +2,7 @@
 	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
 	import { getGameStore } from './store';
+	import { page } from '$app/stores';
 
 	const game = getGameStore();
 
@@ -14,18 +15,24 @@
 	const registerWorker = async () => {
 		if (browser && 'serviceWorker' in navigator) {
 			const SyncWorker = await import('$lib/game/game.worker?worker');
-
+			console.log('register worker');
 			$game.worker = new SyncWorker.default();
 			$game.worker.onmessage = onMsg;
 
 			$game.worker.postMessage({
 				msg: 'start',
-				data: { value: 1000 } //TODO change with settings
+				data: { value: $game.settings.tickspeed }
 			});
+			$page.data.serviceWorkers.push($game.worker);
 		}
 	};
 
 	onMount(() => {
-		if (!$game.worker) registerWorker();
+		console.log('serviceWorkers:', $page.data.serviceWorkers);
+		if (!$page.data.serviceWorkers.length) registerWorker();
+		else {
+			$game.worker = $page.data.serviceWorkers[0];
+			if ($game.worker) $game.worker.onmessage = onMsg;
+		}
 	});
 </script>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,9 @@
+import type { LayoutLoad } from './$types';
+
 export const prerender = true;
+
+const serviceWorkers: Worker[] = [];
+
+export const load: LayoutLoad = async () => {
+	return { serviceWorkers };
+};
