00ab7fa74a880c62eb8776c71a69d2769ac3e49f
Leangeful <cuttheleash1@gmail.com>
2023-11-04 23:49:32 +0100

    added Store component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import Store from '$lib/game/components/Store.svelte';
 	import { getGameStore } from '$lib/game/gameStore.js';
 	import { t } from '$lib/i18n/index.js';
 
@@ -11,12 +12,5 @@
 </script>
 
 {#if $game}
-	<div class="space-y-5 w-full">
-		<h1 class="h1 mt-10">
-			<span>{$t('nav.' + ($game.store.hasOpened ? 'mainStore' : 'mainHome'))}</span>
-		</h1>
-		<hr class="h-px" />
-
-		<p>Gold: {$game?.gold.amount.toFixed(0) ?? 0}</p>
-	</div>
+	<Store />
 {/if}
