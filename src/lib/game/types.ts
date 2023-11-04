b3f26e6807c790299ee383822b293fc8fab696cd
Leangeful <cuttheleash1@gmail.com>
2023-11-04 21:04:04 +0100

    updated game types

--- src/lib/game/types.ts
+++ src/lib/game/types.ts
@@ -1,14 +1,13 @@
-//import type { Settings } from '$lib/settings/types.js';
+import type { SettingsExport } from '$lib/settings/types.js';
+import type { GoldExport } from './ressource/types.js';
 
 export type { GameStore } from './store.js';
 
-export interface GameState {
-	state: {
-		n: number;
-		b: number;
-	};
-
-	//settings: Settings;
-}
-
 export type GameWorker = Worker | undefined;
+
+export interface GameExport {
+	settings: SettingsExport;
+
+	n: number;
+	gold: GoldExport;
+}
