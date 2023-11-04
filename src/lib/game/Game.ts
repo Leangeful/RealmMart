1a04d58f3e918c4d46b1a160b423653c808a0793
Leangeful <cuttheleash1@gmail.com>
2023-11-04 21:05:34 +0100

    update Game class

--- src/lib/game/Game.ts
+++ src/lib/game/Game.ts
@@ -1,47 +1,53 @@
 import { settings } from '$lib/settings/Settings';
-import type { ISettings } from '$lib/settings/types';
+import type { SettingsExport } from '$lib/settings/types';
+import { Gold } from './ressource/Gold';
 import { loadGame, saveGame } from './save_load/save_load';
-import type { GameState, GameWorker } from './types';
+import type { GameExport, GameWorker } from './types';
 
-export class Game implements GameState {
-	state = {
-		n: 0,
-		b: 0
-	};
+export class Game implements GameExport {
+	n = 0;
+	gold = new Gold(0, 0.5);
 
 	lastProcess = Date.now();
 	deltaT = Date.now();
 	timeSinceSave = 0;
 
-	settings: ISettings = settings;
+	settings: SettingsExport = settings;
 	worker: GameWorker;
 
-	constructor(saveGame?: GameState, saveSettings?: ISettings) {
-		if (saveGame) this.state = saveGame.state;
+	process() {
+		this.processTime();
+		this.gold.process(this.deltaT);
+		this.n += 1 / (1000 / this.deltaT);
+		this.timeSinceSave += this.deltaT;
 
-		if (saveSettings) this.settings = saveSettings;
-	}
-
-	load(saveKey: string) {
-		const saveGame = loadGame(saveKey);
-		if (saveGame) Object.assign(this, saveGame);
+		this.save();
 	}
 
 	processTime() {
 		const tCurrent = Date.now();
 		this.deltaT = tCurrent - this.lastProcess;
 		this.lastProcess = tCurrent;
-		this.timeSinceSave += this.deltaT;
 	}
 
-	process() {
-		this.processTime();
+	getExport(): GameExport {
+		return { settings: this.settings, n: this.n, gold: this.gold.getExportedProps() };
+	}
 
-		this.state.n += 1 / (1000 / this.deltaT);
-
+	save() {
 		if (this.timeSinceSave >= settings.autoSaveTime) {
-			saveGame({ state: this.state });
+			saveGame(this.getExport());
 			this.timeSinceSave = 0;
 		}
+	}
+
+	load(saveKey?: string) {
+		const loadedGame = loadGame(saveKey);
+		if (loadedGame) {
+			this.settings = loadedGame.settings;
+			this.n = loadedGame.n;
+			this.gold.setExportedProps(loadedGame.gold);
+		}
+		console.log('Loaded:', loadedGame);
 	}
 }
